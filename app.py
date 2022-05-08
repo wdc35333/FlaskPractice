31b30327fd1aa1a659930975f5cc2d8de2d31e35
wdc35333 <wdc35333@gmail.com>
2022-05-08 15:33:00 +0900

    GET POST 구분

--- app.py
+++ app.py
@@ -1,31 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=('GET', 'POST'))  # 접속하는 url
 def index():
-    return 'Index Page'
+    if request.method == "POST":
+        # user = request.form['user'] # 전달받은 name이 user인 데이터
+        print(request.form.get('user')) # 안전하게 가져오려면 get
+        user = request.form.get('user')
+        data = {'level': 60, 'point': 360, 'exp': 45000}
+        return render_template('index.html', user=user, data=data)
+    elif request.method == "GET":
+        user = "반원"
+        data = {'level': 60, 'point': 360, 'exp': 45000}
+        return render_template('index.html', user=user, data=data)
 
-@app.route('/hello')
-def hello():
-    return 'Hello World'
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-
-@app.route('/projects/')
-def projects():
-    return 'The project page'
-
-@app.route('/about')
-def about():
-    return 'The about page'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+    # host 등을 지정하고 싶다면
+    # app.run(host="127.0.0.1", port = "5000", debug=True)
